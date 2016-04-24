c0ea53ca7a40afe6a735182c0171d60f89ab10ae
Hyunchel Kim <hyunchelkk@gmail.com>
2016-04-23 22:40:55 -0500

    Save each square into jpg file

--- src/ImageProcessor.cpp
+++ src/ImageProcessor.cpp
@@ -16,55 +16,21 @@ double ImageProcessor::FindCosine(const Point& pt1, const Point& pt2, const Poin
 }
 
 void ImageProcessor::PopulateBoard() {
-    int thresh = 500, N = 300;
-	vector<vector<Point>> squares;
-	Mat pyr, timg, gray0(_sudokuImage.size(), CV_8U), gray;
+    
+    int rowSize = _sudokuImage.rows / 9;
+    int colSize = _sudokuImage.cols / 9;
+    int rowBlockSize, colBlockSize;
 
-    //pyrDown(_sudokuImage, pyr, Size(_sudokuImage.cols/2, _sudokuImage.rows/2));
-    //pyrUp(pyr, timg, _sudokuImage.size());
-    vector<vector<Point> > contours;
-
-    for(int c = 0; c < 3; c++) {
-        int ch[] = {c, 0};
-        mixChannels(&_sudokuImage, 1, &gray0, 1, ch, 1);
-
-        for(int l = 0; l < N; l++) {
-            if(l == 0) {
-                Canny(gray0, gray, 0, thresh, 7);
-                dilate(gray, gray, Mat(), Point(-1,-1));
-            } else {
-                gray = gray0 >= (l+1)*255/N;
-            }
-
-            findContours(gray, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
-            vector<Point> approx;
-
-            for(size_t i = 0; i < contours.size(); i++ ) {
-                approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true)*0.02, true);
-
-                if(approx.size() == 4 &&
-                    fabs(contourArea(Mat(approx))) > 1000 &&
-                    isContourConvex(Mat(approx))) {
-                    double maxCosine = 0;
-
-                    for(int j = 2; j < 5; j++) {
-                        double cosine = fabs(FindCosine(approx[j%4], approx[j-2], approx[j-1]));
-                        maxCosine = MAX(maxCosine, cosine);
-                    }
-
-                    if(maxCosine < 0.3)
-                        squares.push_back(approx);
-                }
-            }
+    cout << "Approx. size of a square: " << rowSize << ", " << colSize << endl;
+    
+    for (int i=0; i<9; i++) {
+        rowBlockSize = rowSize * i;
+        for (int j=0; j<9; j++) {
+            colBlockSize = colSize * j;
+            string filename = "box" + to_string(i) + to_string(j) + ".jpg";
+            Rect box = Rect(rowBlockSize, colBlockSize, rowSize, colSize);
+            imwrite(filename, _sudokuImage(box));
         }
-    }
-
-    cout << squares.size() << endl;
-
-    for(size_t i = 0; i < squares.size(); i++) {
-        const Point* p = &squares[i][0];
-        int n = (int)squares[i].size();
-        polylines(_sudokuImage, &p, &n, 1, true, Scalar(0,255,0), 3, LINE_AA);
     }
 }
 
